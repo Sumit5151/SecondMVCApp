babd8ab9580b404eee1443b43496689d29947487
Sumit5151 <sumitshitole5151@gmail.com>
2022-11-10 09:08:46 +0530

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecondMVCApp.Models;
+
+namespace SecondMVCApp.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        //[HttpGet]
+        //public IActionResult Login()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public IActionResult Login(string userName, string password)
+        //{
+
+        //    if(userName == "Bright" && password == "Future")
+        //    {
+        //        return RedirectToAction("Dashboard");
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("Login");
+        //    }
+        //    return View();
+        //}
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            User user = new User();
+            user.ClassName = "Test";
+            return View(user);
+        }
+
+       [HttpPost]
+        public IActionResult Login(User user)
+        {
+
+            if(user.UserName == "Test" && user.Password =="test")
+            {
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+
+
+        
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace SecondMVCApp.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ClassName { get; set; } 
+    }
+}
