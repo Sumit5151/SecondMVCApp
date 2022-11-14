0412aafb515b1630cbba0c220b13cde8c9042c48
Sumit5151 <sumitshitole5151@gmail.com>
2022-11-14 09:05:20 +0530

    Added user controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecondMVCApp.Models;
+
+namespace SecondMVCApp.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public static List<User> Users;
+
+        static UserController()
+        {
+            Users = new List<User>();
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(Users);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+
+            Users.Add(user);
+
+            return RedirectToAction("Index");
+
+        }
+
+
+
+        public IActionResult Delete(int id)
+        {
+
+            var user = Users.FirstOrDefault(user=>user.Id == id);
+
+
+            Users.Remove(user);
+
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Update(User user)
+        {
+            return View();
+        }
+
+
+
+
+
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -2,8 +2,12 @@
 {
     public class User
     {
+
+        public int Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public string ClassName { get; set; } 
+
+
     }
 }
